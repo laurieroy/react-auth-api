395488ca55279295ff62a8b2b97c6cea76c10ed5
lauretteroy <laurieroy.dev@gmail.com>
2021-12-21 13:17:44 -0800

    Add Registration, update routes

---
+++ src/components/auth/Registration.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Registration extends Component {
+  constr
+  render() {
+    return <div>Registration goes here</div>;
+  }
+}
+
+export default Registration;
